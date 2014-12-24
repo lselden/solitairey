809764069f9c01ad451574aa3e2b3b32b0b28435
sophstad <sstadler96@verizon.net>
2014-12-24 18:13:51 -0500

    upload dots theme

--- js/application.js
+++ js/application.js
@@ -475,14 +475,13 @@
 				sizes: [148],
 				148: {
 					hiddenRankHeight: 17,
-					rankHeight: 55,
+					rankHeight: 50,
 					dimensions: [148, 200]
 				}
 			},
-		},
 
 		current: null,
-		defaultTheme: "dots",
+		defaultTheme: "jolly-royal",
 
 		/* theres no mechanism yet to load the appropriate deck depending on the scaled card width
 		 * so we just load the largest cards and call it a day :/
