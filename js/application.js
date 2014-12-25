001b80615d0f8dadfc864b8ca1b92e4fc617bd0d
sophstad <sstadler96@verizon.net>
2014-12-24 19:28:29 -0500

    Revert c286866..b3dce4c
    This rolls back to commit c28686691c1312096de257f6fc6020d3570e5efa.

--- js/application.js
+++ js/application.js
@@ -481,9 +481,9 @@
 			},
 
 		current: null,
-		defaultTheme: "dots",
+		defaultTheme: "jolly-royal",
 
-		/* there's no mechanism yet to load the appropriate deck depending on the scaled card width
+		/* theres no mechanism yet to load the appropriate deck depending on the scaled card width
 		 * so we just load the largest cards and call it a day :/
 		 */
 		snapToSize: function (width) {
